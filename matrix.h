00b53eb88e05fe59e692ef5a6d49756376a46236
Iyad Obeid <iobeid@temple.edu>
2026-02-06 20:19:50 -0500

    added operator== to matrix.h

--- matrix.h
+++ matrix.h
@@ -25,5 +25,6 @@ class Matrix{
     Matrix operator-( const float& )  const;
     Matrix operator*( const float& )  const;
     Matrix operator/( const float& )  const;
+    Matrix operator==(const Matrix& ) const;    // determine if two matrices are equal
 
 };
