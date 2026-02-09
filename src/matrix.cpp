945488495b5548d13c736442dfe39d6b1bb05eda
Jennifer Lam <tuq47389@ece-000.eng.temple.edu>
2026-02-09 13:53:14 -0500

    determinant added!

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,4 +1,5 @@
 #include "../matrix.h"
+#include <stdexcept>
 
 
 Matrix Matrix::operator+(const float& k) const {
@@ -48,3 +49,48 @@ Matrix Matrix::operator/(const float& k) const {
     delete[] vals;
     return result;
 }
+
+
+float Matrix::determinant() const {
+
+    if (rows != cols) {
+        throw std::invalid_argument("determinant requires square matrix!!!");
+    }
+
+    // 1x1
+    if (rows == 1) {
+        return (*this)(0,0);
+    }
+
+    // 2x2
+    if (rows == 2) {
+        return (*this)(0,0) * (*this)(1,1)
+             - (*this)(0,1) * (*this)(1,0);
+    }
+
+    float det = 0.0f;
+
+    // cofactor expansion along first row
+    for (int j = 0; j < cols; j++) {
+
+        float* minor_vals = new float[(rows - 1) * (cols - 1)];
+        int k = 0;
+
+        for (int i = 1; i < rows; i++) {
+            for (int c = 0; c < cols; c++) {
+                if (c != j) {
+                    minor_vals[k++] = (*this)(i, c);
+                }
+            }
+        }
+
+        Matrix minor(rows - 1, cols - 1, minor_vals);
+        delete[] minor_vals;
+
+        float sign = (j % 2 == 0) ? 1.0f : -1.0f;
+        det += sign * (*this)(0, j) * minor.determinant();
+    }
+
+    return det;
+}
+
