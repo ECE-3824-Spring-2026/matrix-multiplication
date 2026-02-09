c43fea01f8d0dd1397684ff808b6353d520926ac
adil_chariwala <adilchariwala@gmail.com>
2026-02-09 13:17:53 -0500

    1st test

--- testing/test_transpose.cpp
+++ testing/test_transpose.cpp
@@ -1,2 +1,17 @@
+
 // adil chariwala 
-// test 2
+
+#include <catch2/catch_test_macros.hpp>
+#include <cmath>
+#include <stdexcept>
+#include "../matrix.h"
+
+TEST_CASE("0 based index, and matches doc"){
+    // x = [[1,2,3],[4,5,6]]  (2x3)
+    float vals[] = {1,2,3,4,5,6};
+    Matrix x(2, 3, vals);
+
+    REQUIRE(x(1,2) == 6.0f);
+    REQUIRE(x(0,0) == 1.0f);
+    REQUIRE(x(1,0) == 4.0f);
+}
