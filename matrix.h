1b2c91b814035a5de65f5d6a0c13bb9f0f958e7e
Chanyoung Kim <02paul35@gmail.com>
2026-02-12 04:36:31 +0900

    Add internal storage (rows, cols, data) to Matrix

--- matrix.h
+++ matrix.h
@@ -1,30 +1,36 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
 class Matrix{
-    private:
+private:
+    int rows;
+    int cols;
+    float* data;
 
-    public:
+public:
+    Matrix(int,int,const float*);
+    Matrix& operator=(const Matrix&);
+    ~Matrix();
 
-    Matrix(int,int,const float*);           // constructor
-    Matrix& operator=(const Matrix&);       // copy assignment A = B
-    ~Matrix();                              // destructor
+    const float& operator()(int,int) const;
+    float& operator()(int,int);
 
-    const float& operator()(int,int) const; // enables A(1,2) = 3.14 -> data assignment 
-    float& operator()(int i, int j);        // enables float x = A(1,2) -> data retrieval
+    int get_n_rows() const;
+    int get_n_cols() const;
+    float determinant() const;
+    int rank() const;
+    void display() const;
 
-    int get_n_rows() const;                 // return number of rows
-    int get_n_cols() const;                 // return number of columns
-    float determinant() const;              // return matrix determinant
-    int rank() const;                       // return matrix rank
-    void display() const;                   // display formatted matrix
+    Matrix transpose() const;
 
-    Matrix transpose() const;                   // B = A.transpose
-
-    Matrix operator+( const Matrix& ) const;    // add Matrix plus Matrix
-    Matrix operator-( const Matrix& ) const;
-    Matrix operator*( const Matrix& ) const;
-    Matrix operator+( const float& )  const;    // add constant to Matrix
-    Matrix operator-( const float& )  const;
-    Matrix operator*( const float& )  const;
-    Matrix operator/( const float& )  const;
-    bool operator==(const Matrix& ) const;    // determine if two matrices are equal
-
+    Matrix operator+(const Matrix&) const;
+    Matrix operator-(const Matrix&) const;
+    Matrix operator*(const Matrix&) const;
+    Matrix operator+(const float&) const;
+    Matrix operator-(const float&) const;
+    Matrix operator*(const float&) const;
+    Matrix operator/(const float&) const;
+    bool operator==(const Matrix&) const;
 };
+
+#endif
