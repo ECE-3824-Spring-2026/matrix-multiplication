c91f35e12cf8fceb8d955e6087b64ca1c36dbdb2
mariyadenny <dennymariya7@gmail.com>
2026-02-13 13:21:21 -0500

    made a few edits

--- testing/test_float_divide.cpp
+++ testing/test_float_divide.cpp
@@ -17,10 +17,10 @@ TEST_CASE("Scalar division with basic positive number"){
     Matrix B = A / 2.0f;
     
     //this checks if each element is approx equal to the quotient
-    REQUIRE(B(0,0) == Approx(1.0f));// 2/2=1
-    REQUIRE(B(0,1) == Approx(2.0f));// 4/2=2
-    REQUIRE(B(1,0) == Approx(3.0f));// 6/2=3
-    REQUIRE(B(1,1) == Approx(4.0f));// 8/2=4
+    REQUIRE(B(0,0) == (1.0f));// 2/2=1
+    REQUIRE(B(0,1) == (2.0f));// 4/2=2
+    REQUIRE(B(1,0) == (3.0f));// 6/2=3
+    REQUIRE(B(1,1) == (4.0f));// 8/2=4
 }
 
 TEST_CASE("Scalar division with negative number"){
@@ -34,10 +34,10 @@ TEST_CASE("Scalar division with negative number"){
     Matrix B = A / -2.0f;
     
     //this checks if each element is approx equal to the quotient
-    REQUIRE(B(0,0) == Approx(-1.0f));// 2/-2=-1
-    REQUIRE(B(0,1) == Approx(2.0f));// -4/-2=2
-    REQUIRE(B(1,0) == Approx(-3.0f));// 6/-2=-3
-    REQUIRE(B(1,1) == Approx(4.0f));// -8/-2=4
+    REQUIRE(B(0,0) == (-1.0f));// 2/-2=-1
+    REQUIRE(B(0,1) == (2.0f));// -4/-2=2
+    REQUIRE(B(1,0) == (-3.0f));// 6/-2=-3
+    REQUIRE(B(1,1) == (4.0f));// -8/-2=4
 }
 
 TEST_CASE("Scalar division by zero throws domain_error"){
@@ -56,20 +56,20 @@ TEST_CASE("Scalar division by Decimal number"){
     // Matrix B would store the result of each element of matrix A divided by 0.5
     Matrix B = A / 0.5f;
 
-    REQUIRE(B(0,0) == Approx(2.0f));// 1/0.5=2
-    REQUIRE(B(0,1) == Approx(4.0f));// 2/0.5=4
-    REQUIRE(B(1,0) == Approx(6.0f));// 3/0.5=6
-    REQUIRE(B(1,1) == Approx(8.0f));// 4/0.5=8
+    REQUIRE(B(0,0) == (2.0f));// 1/0.5=2
+    REQUIRE(B(0,1) == (4.0f));// 2/0.5=4
+    REQUIRE(B(1,0) == (6.0f));// 3/0.5=6
+    REQUIRE(B(1,1) == (8.0f));// 4/0.5=8
 }
 
 TEST_CASE("Scalar division with negative decimal") {
     float vals[] = {1, -2, 3, -4};
     Matrix A(2,2,vals);
     Matrix B = A / -0.5f; // divide by -0.5 = multiply by -2
-    REQUIRE(B(0,0) == Approx(-2.0f));
-    REQUIRE(B(0,1) == Approx(4.0f));
-    REQUIRE(B(1,0) == Approx(-6.0f));
-    REQUIRE(B(1,1) == Approx(8.0f));
+    REQUIRE(B(0,0) == (-2.0f));
+    REQUIRE(B(0,1) == (4.0f));
+    REQUIRE(B(1,0) == (-6.0f));
+    REQUIRE(B(1,1) == (8.0f));
 }
 
 TEST_CASE("Scalar division Does not modify original matrix"){
@@ -80,8 +80,8 @@ TEST_CASE("Scalar division Does not modify original matrix"){
     // Matrix B would store the result of each element of matrix A divided by 3.0
     Matrix B = A / 3.0f;
 
-    REQUIRE(A(0,0) == Approx(3.0f));// confirms A(0,0)=3
-    REQUIRE(A(0,1) == Approx(6.0f));// confirms A(0,1)=6
-    REQUIRE(A(1,0) == Approx(9.0f));// confirms A(1,0)=9
-    REQUIRE(A(1,1) == Approx(12.0f));// confirms A(1,1)=12
+    REQUIRE(A(0,0) == (3.0f));// confirms A(0,0)=3
+    REQUIRE(A(0,1) == (6.0f));// confirms A(0,1)=6
+    REQUIRE(A(1,0) == (9.0f));// confirms A(1,0)=9
+    REQUIRE(A(1,1) == (12.0f));// confirms A(1,1)=12
 }
