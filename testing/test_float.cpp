e698af35ee35b8aad7713d37e1afc1f7f563a0c8
mariyadenny <dennymariya7@gmail.com>
2026-02-12 20:05:56 -0500

    added two more test cases for multiplication with decimal numbers and no changes with original matrix after multiplication

--- testing/test_float.cpp
+++ testing/test_float.cpp
@@ -29,7 +29,8 @@ TEST_CASE("Scalar multiplication with zero"){
     Matrix A(2,2,vals);
     
     //The new matrix B will have the result of the product of the element in A matrix by zero
-    Matrix B =A*0.0f; 
+    Matrix B = A*0.0f; 
+    //the following lines checks if each element is approx equal to the product of element and zero
     REQUIRE(B(0,0) == Approx(0.0f)); //5*0=0
     REQUIRE(B(0,1) == Approx(0.0f)); //-3*0=0
     REQUIRE(B(1,0) == Approx(0.0f)); //2*0=0
@@ -39,11 +40,36 @@ TEST_CASE("Scalar multiplication with zero"){
 
 TEST_CASE("Scalar Multiplication  with a negative number"){
     float vals[]={1,-2,3,-4};
-    MatrixA(2,2,vals);
+    Matrix A(2,2,vals);
     //this new matrix will be the product of elements in A matrix with a negative number
-    Matrix B =A*-3.0f;
+    Matrix B = A * -3.0f;
+    //the following lines checks if each element is approx equal to the product of element and the negative number
     REQUIRE(B(0,0) == Approx(-3.0f)); //1*-3=-3
     REQUIRE(B(0,1) == Approx(6.0f)); //-2*-3=6
     REQUIRE(B(1,0) == Approx(-9.0f)); //3*-3=-9
     REQUIRE(B(1,1) == Approx(12.0f));//-4*-3=12
+}
+
+TEST_CASE("Scalar Multiplication with decimal numbers"){
+    float vals[]={2,4,6,8};
+    Matrix A(2,2,vals);
+    //this new matrix will be the product of elements in A matrix with a decimal number
+    Matrix B = A*0.5f;
+    //the following lines checks if each element is approx equal to the product of element and the decimal number
+    REQUIRE(B(0,0)== Approx(1.0f));//2*0.5=1
+    REQUIRE(B(0,1)== Approx(2.0f));//4*0.5=2
+    REQUIRE(B(1,0)== Approx(3.0f));//6*0.5=3
+    REQUIRE(B(1,1)== Approx(4.0f));//8*0.5=4
+}
+
+TEST_CASE("Scalar multiplication does not modify original matrix"){
+    float vals[]={1,2,3,4};
+    Matrix A(2,2,vals);
+    //this new matrix will be the product
+    Matrix B = A* 5.0f;
+    //the following lines checks whether the original matrix stays the same after multiplication
+    REQUIRE(A(0,0)== Approx(1.0f));// confirms A(0,0) is still 1
+    REQUIRE(A(0,1)== Approx(2.0f));//confirms that A(0,1) is still 2
+    REQUIRE(A(1,0)== Approx(3.0f));//A(1,0) is still 3
+    REQUIRE(A(1,1)== Approx(4.0f));//A(1,1) is still 4
 }
