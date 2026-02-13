43eaf778e8dc873e3a76db2f0a6eef72bd7bbc55
mariyadenny <dennymariya7@gmail.com>
2026-02-12 21:19:59 -0500

    Added 2 new test cases for matrix division by a decimal number and that it doesn't affect the original matrix after division

--- testing/test_float_divide.cpp
+++ testing/test_float_divide.cpp
@@ -47,3 +47,32 @@ TEST_CASE("Scalar division by zero throws domain_error"){
     // requires domain_error on divide by zero
     REQUIRE_THROWS_AS(A / 0.0f, std::domain_error);
 }
+
+TEST_CASE("Scalar division by Decimal number"){
+    //array of 4 elements to fill the matrix
+    float vals[] = {1, 2, 3, 4};
+    Matrix A(2, 2, vals);
+    
+    // Matrix B would store the result of each element of matrix A divided by 0.5
+    Matrix B = A / 0.5f;
+
+    REQUIRE(B(0,0) == Approx(2.0f));// 1/0.5=2
+    REQUIRE(B(0,1) == Approx(4.0f));// 2/0.5=4
+    REQUIRE(B(1,0) == Approx(6.0f));// 3/0.5=6
+    REQUIRE(B(1,1) == Approx(8.0f));// 4/0.5=8
+}
+
+
+TEST_CASE("Scalar division Does not modify original matrix"){
+    //array of 4 elements to fill the matrix
+    float vals[] = {3, 6, 9, 12};
+    Matrix A(2, 2, vals);
+    
+    // Matrix B would store the result of each element of matrix A divided by 3.0
+    Matrix B = A / 3.0f;
+
+    REQUIRE(A(0,0) == Approx(3.0f));// confirms A(0,0)=3
+    REQUIRE(A(0,1) == Approx(6.0f));// confirms A(0,1)=6
+    REQUIRE(A(1,0) == Approx(9.0f));// confirms A(1,0)=9
+    REQUIRE(A(1,1) == Approx(12.0f));// confirms A(1,1)=12
+}
